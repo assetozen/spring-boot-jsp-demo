27d9b71697df5e5086866f8fae48aaad98bc4f45
assetozen <assetozen@gmail.com>
2020-03-31 11:43:13 +0200

    First commit

--- src/main/java/com/assegd/app/ws/security/WebSecurity.java
+++ src/main/java/com/assegd/app/ws/security/WebSecurity.java
@@ -12,26 +12,34 @@ import javax.swing.*;
 
 
 @EnableWebSecurity
-public class WebSecurity  extends WebSecurityConfigurerAdapter {
+public class WebSecurity extends WebSecurityConfigurerAdapter {
     private final UserService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public WebSecurity(UserService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder){
+    public WebSecurity(UserService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userDetailsService = userDetailsService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws  Exception {
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
-                .permitAll().anyRequest()
-                .authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()));
+                .permitAll()
+                .anyRequest().authenticated().and()
+                .addFilter(getAuthenticationFilter())
+                .addFilter(new AuthorizationFilter(authenticationManager()));
     }
 
     @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception{
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    public AuthenticationFilter getAuthenticationFilter() throws Exception {
+        final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+        filter.setFilterProcessesUrl("/users/login");
+        return filter;
     }
 }
